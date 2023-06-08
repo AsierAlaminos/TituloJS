42e5e4079e7e2a73a3881ca16b66a49c703fff75
Asier <asmus@localhost.localdomain>
2023-06-08 18:33:04 +0200

    update

---
+++ inicio/03_dinamismo/utils/js/01_nodos.js
@@ -0,0 +1,69 @@
+//Busqueda de elementos por js
+
+//Busqueda por Id
+/*let nodoBoton = document.getElementById("botonPulsar");
+console.log(nodoBoton);
+
+//Busqueda por Clase
+let botonClass = document.getElementsByClassName("btn");
+console.log(botonClass);
+
+//Busqueda por Tag/Etiqueta
+let botonTag = document.getElementsByTagName("button");
+console.log(botonTag);
+
+//Busqueda general
+//Si no se especifica busca por Tag
+//Si pones . buscas por clase
+//Si pones # buscas por id
+let nodoQuery = document.querySelector(".btn");
+console.log(nodoQuery);*/
+
+let botonPulsar = document.querySelector("#botonPulsar");
+
+let botonBorrar = document.querySelector("#botonBorrar");
+
+//Si en el Selector pones esto -> div.lista-dos la etiqueta tiene que cumplir con la   clase y sería igual para los id -> div#lista-dos
+
+/*let li1 = document.querySelectorAll("div.lista-uno ul li");
+let li2 = document.querySelectorAll("div.lista-dos ul li");*/
+//console.log(li);
+let lista2 = document.querySelector("div.lista-dos #lista");
+
+let lista1 = document.querySelector("div.lista-uno #lista");
+
+let input = document.querySelector("#input_nombre");
+
+let select = document.querySelector("#select");
+
+let li = document.querySelectorAll("li");
+
+botonPulsar.addEventListener("click", (e) => {
+	/*let nodoLi = document.createElement("li");
+	nodoLi.className = "list-group-item li-hijo";
+	console.log(input.value);
+	nodoLi.textContent = input.value;
+	input.value = "";
+	lista.append(nodoLi);*/
+	if (select.value == 1)
+		lista1.innerHTML += `<li class='list-group-item li-hijo'>${input.value}</li>`;
+	else
+		lista2.innerHTML += `<li class='list-group-item li-hijo'>${input.value}</li>`;
+});
+
+console.log(li);
+let pulsado;
+
+li.forEach(e => {
+	e.addEventListener("click", () => {
+		li.forEach(e1 => {
+			e1.classList.remove("active");
+		});
+		e.classList.add("active");
+		pulsado = e;
+	});
+});
+
+botonBorrar.addEventListener("click", (e) => {
+	pulsado.remove();
+});
