0234ab871ef4c67aabaab863fd0ce819cdb38cfd
Asier <asmus@localhost.localdomain>
2023-06-17 20:43:13 +0200

    update

--- proyecto_final/utils/js/detalles.js
+++ proyecto_final/utils/js/detalles.js
@@ -6,28 +6,32 @@ function generarDetalles() {
 	fetch(`https://pokeapi.co/api/v2/pokemon/${localStorage.getItem("id-pokemon")}`)
 	.then((e) => e.json())
 	.then((e1) => {
-		containerDetalles.innerHTML += `<div class="row"> 
+		containerDetalles.innerHTML += `
 							<div class="col-3">
 							<img src="${e1.sprites.front_default}" style="width: 200px">
-							</div>
-							<div class="col-3">
 							<h2>${e1.forms[0].name}</h2>
 							</div>
 							<div class="col-3" id="tipos">
-								<h3>Tipos</h3>
+								<h3>Tipo</h3>
 							</div>
 							<div class="col-3" id="stats">
 								<h3>Stats</h3>
 							</div>
-						</div>`;
+							<div class="col-3" id="abilities">
+								<h3>Abilities</h3>
+							</div>
+							`;
 		let divTipos = document.querySelector("#tipos");
 		let divStats = document.querySelector("#stats");
+		let divAbilities = document.querySelector("#abilities");
 		e1.types.forEach(element => {
 			divTipos.innerHTML += `<h4>${element.type.name}</h4>`;
 		});
 		e1.stats.forEach(element => {
-			console.log(element);
 			divStats.innerHTML += `<h4>${element.stat.name}: ${element.base_stat}</h4>`;
+		});
+		e1.abilities.forEach(element => {
+			divAbilities.innerHTML += `<h4>${element.ability.name}</h4>`;
 		});
 	});
 }
